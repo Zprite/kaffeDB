3c9966cfc35432772d7e26e66247932e8574664c
mbbirkel <mbbirkel@stud.ntnu.no>
2022-03-16 16:16:00 +0100

    add more sample data!

--- KaffeDB.sql
+++ KaffeDB.sql
@@ -84,32 +84,80 @@ CREATE TABLE DyrketKaffebønne (
     CONSTRAINT DyrketKaffebønne_FK3 FOREIGN KEY (KaffepartiID) REFERENCES Kaffeparti(ID)
 );
 
-/*Brukerhistorie 1*/
+/* Populate tables*/
+
 INSERT INTO Kaffebønne (Art,Navn)
 VALUES ("Coffea Arabica", "Bourbon"),
 ("Coffea Robusta", "Robusta"),
 ("Coffea Liberica", "Liberica");
 
 INSERT INTO Foredlingsmetode (Navn, Beskrivelse)
-VALUES ("Bærtørket", "Hele Kaffebæret tørkes. 
+VALUES ("Bærtørket", 
+"Hele Kaffebæret tørkes. 
 Med denne prosesseringen fjernes verken skallet eller fruktkjøttet før tørking. 
-Denne prosesseringen gir ofte mer fyldige kaffe med stor munnfølelse. Denne prosesseringen kalles også for Natural.");
+Denne prosesseringen gir ofte mer fyldige kaffe med stor munnfølelse. Denne prosesseringen kalles også for Natural."),
+("Vasket",
+ "Den våte metode går ut på å skille fruktkjøttet fra bønnen i en maskinell vaskeprosess umiddelbart etter innhøsting.
+ Kaffen omtales gjerne som vasket eller våtforedlet. 
+Den våte metode benyttes blant annet i flere land i Sør- og Mellom-Amerika, samt i Øst-Afrika.");
 
 INSERT INTO Gård (Navn, HøydeOverHavet, Region, Land)
-Values ("Nombre de Dios", 1500, "Santa Ana", "El Salvador");
+Values ("Nombre de Dios", 1500, "Santa Ana", "El Salvador"),
+("Don Eduardo", 800, "Quindo", "Colombia"),
+("Finca Victoria",1200, "Magdalena", "Colombia"),
+("Rwanda Farmers Coffee Company", 200, "Kigali", "Rwanda"),
+("Sake Farm", 352, "Gafunzo", "Rwanda");
 
 INSERT INTO Kaffeparti(Innhøstingsår, Betalt, GårdID, ForedlingsmetodeNavn)
-VALUES (2021, 8, (SELECT ID from Gård WHERE Navn='Nombre de Dios') , "Bærtørket");
+VALUES (2021, 8, (SELECT ID from Gård WHERE Navn='Nombre de Dios') , "Bærtørket"),
+(2022, 6, 2, "Vasket"),
+(2020, 5.5, 3, "Bærtørket"),
+(2020, 5, 4, "Vasket"),
+(2021, 6.5, 5, "Bærtørket");
+
 
 INSERT INTO DyrketKaffebønne (Art, GårdID, KaffepartiID)
-VALUES ("Coffea Arabica",
-(SELECT ID from Gård WHERE Navn='Nombre de Dios'),
-(SELECT ID from Kaffeparti  WHERE (Innhøstingsår=2021 and Betalt=8)));
+VALUES (
+    "Coffea Arabica",
+    1,
+    (SELECT ID from Kaffeparti  WHERE (Innhøstingsår=2021 and Betalt=8))
+),
+(
+    "Coffea Robusta",
+    2,
+    (SELECT ID from Kaffeparti  WHERE (Innhøstingsår=2021 and Betalt=6.5))
+),
+(
+    "Coffea Arabica",
+    3,
+    (SELECT ID from Kaffeparti  WHERE (Innhøstingsår=2021 and Betalt=6.5))
+),
+(
+    "Coffea Liberica",
+    4,
+    (SELECT ID from Kaffeparti  WHERE (Innhøstingsår=2020 and Betalt=5))
+),
+(
+    "Coffea Arabica",
+    5,
+    (SELECT ID from Kaffeparti  WHERE (Innhøstingsår=2020 and Betalt=5.5))
+);
 
 INSERT INTO Kaffebrenneri(Navn, Lokasjon)
-VALUES ("Jacobsen & Svart", "Trondheim");
+VALUES ("Jacobsen & Svart", "Trondheim"),
+("Jacobsen & Svart", "Oslo"),
+("Baker Brun", "Bergen"),
+("Bergen Kaffebrenneri", "Bergen"),
+("Kaffebrenneriet", "Trondheim"),
+("Kaffebrenneriet", "Oslo"),
+("Kaffebrenneriet", "Asker");
 
+/* Brukerhistorie 1 */
 INSERT INTO FerdigbrentKaffe(Navn, Dato, Beskrivelse, Brenningsgrad, Kilopris, KaffepartiID, KaffebrenneriID)
 VALUES ("Vinterkaffe 2022", "2022-01-20", "En velsmakende og kompleks kaffe for mørketiden", "Lys", 600, 
 (SELECT ID from Kaffeparti WHERE (Innhøstingsår=2021 and Betalt=8)),
-(SELECT ID from Kaffebrenneri WHERE (Navn="Jacobsen & Svart" and Lokasjon="Trondheim")));
+(SELECT ID from Kaffebrenneri WHERE (Navn="Jacobsen & Svart" and Lokasjon="Trondheim"))),
+
+("Vårkaffe 2022", "2022-03-20", "Frisk og fyldig kaffe med en floral smak som hører våren til", "Middels", 550, 1, 5);
+
+
